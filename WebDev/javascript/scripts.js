1cad65ebce5b4e516920012257a438adb7067768
nbg-cordeiro <251023184@aluno.unb.br>
2025-12-18 13:51:31 -0300

    Add first JavaScript script Add small chess table to test JS

---
+++ WebDev/javascript/scripts.js
@@ -0,0 +1,18 @@
+function squares(){
+    for(let i=0;i<10;i++){
+        let text="";
+        for(let i=0;i<8;i++){
+            for(let j=0;j<8;j++){
+                text+='<div class="square';
+                if((i+j)%2 == 0){
+                    text+=' white';
+                }
+                else{
+                    text+=' black';
+                }
+                text+='"></div>'
+            }
+        }
+        document.getElementsByClassName("table")[0].innerHTML = text;
+    }
+}let 
