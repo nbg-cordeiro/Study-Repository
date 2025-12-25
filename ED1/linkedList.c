c418cd991fab7c95dbca2cbca6191f3e90847318
nbg-cordeiro <251023184@aluno.unb.br>
2025-12-24 22:17:07 -0300

    Simplify linkedList

--- ED1/linkedList.c
+++ ED1/linkedList.c
@@ -7,17 +7,16 @@ typedef struct Node{
 }Node;
 
 Node* createNode (int);
-void append (Node**, int);
-void printList(Node*);
-void freeList(Node**);
+void append(Node**, int);
 void removeValue(Node**, int);
-void removeValueOpt(Node**, int);
+void printList(Node**);
+void freeList(Node**);
 
 int main (void)
 {
     Node* head = createNode(0);
     printf("Before appending:\n");
-    printList(head);
+    printList(&head);
     
     for(int i=1; i < 5; i++){
         append(&head,10*i);
@@ -25,89 +24,60 @@ int main (void)
     }
 
     printf("\nAfter appending:\n");
-    printList(head);
+    printList(&head);
 
-    printf("\nAfter removing:\n");
     removeValue(&head,20);
-    printList(head);
+    printf("\nAfter removing:\n");
+    printList(&head);
 
     freeList(&head);
     return 0;
 }
 
-Node* createNode(int val){
-    Node* newNode= malloc(sizeof(Node));
-    newNode->value=val;
+Node* createNode(int value)
+{
+    Node* newNode = malloc(sizeof(Node));
     newNode->next=NULL;
+    newNode->value=value;
     return newNode;
 }
 
-void append(Node** headRef, int val){
-    Node* atual = (*headRef);
-
-    if(atual){
-        while(atual->next != NULL){
-            atual = atual->next;
-        }   
-        atual->next=createNode(val);
-    }
-    else{
-        (*headRef)=createNode(val);
-    }
-}
-
-void printList(Node* head)
+void append(Node** headRef, int value)
 {
-    Node* atual;
-    atual=head;
+    while(*headRef != NULL)
+        headRef = &((*headRef)->next);
+    *headRef=createNode(value);
+}
 
-    while(atual!=NULL){
-        printf("%d\n",atual->value);
-        atual=atual->next;
+void removeValue(Node** headRef, int value)
+{
+    while(*headRef!=NULL){
+        Node* saver = *headRef;
+
+        if(saver->value==value)
+        {
+            *headRef=saver->next;
+            free(saver);
+        }else
+            headRef=&(saver->next);
     }
 }
 
-void freeList(Node** headRef){
-    Node* atual = *headRef;
-    Node* next;
-    while(atual!=NULL)
+void freeList(Node** headRef)
+{
+    while((*headRef)!=NULL)
     {
-        next=atual->next;
-        free(atual);
-        atual=next;
-    }
-    *headRef=NULL;
-}
-
-void removeValue(Node** headRef, int value){
-    Node* previous = NULL;
-    Node* atual = (*headRef);
-    while(atual!=NULL){
-        if(atual->value==value){
-            Node* temp = atual;
-            atual=atual->next;
-            if(previous!=NULL){
-                previous->next=atual;
-            }
-            else{
-                (*headRef)=atual;
-            }
-            free(temp);
-        }else{
-            previous=atual;
-            atual=atual->next;
-        }
+        Node* saver = (*headRef)->next;
+        free(*headRef);
+        *headRef = saver;
     }
 }
 
-//Optimized version of the removeValue function
-void removeValueOpt(Node** headRef,int value){
-    Node* atual = *headRef;
-    if(atual->value==value){
-        *headRef=atual->next;
-        free(atual);
-    }
-    else{
-        headRef= &(atual->next);
+void printList(Node** headRef)
+{
+    while((*headRef)!=NULL)
+    {
+        printf("%d\n",(*headRef)->value);
+        headRef=&((*headRef)->next);
     }
 }
