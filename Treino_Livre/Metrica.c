883e305dfb1df3f6f28fc8496a196429d521a43c
nbg-cordeiro <251023184@aluno.unb.br>
2025-12-27 18:28:49 -0300

    Add 3 MOJ exercises

---
+++ Treino_Livre/Metrica.c
@@ -0,0 +1,16 @@
+// enunciado:
+// https://moj.naquadah.com.br/new/treino/problem/?id=moj-problems.edson-1173
+
+#include <stdio.h>
+
+int main ()
+{
+    static int set[100001];
+    int n;
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++){
+        
+    }
+
+}
